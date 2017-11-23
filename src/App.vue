71cac931e2817b2e83122c0317001596ff06a5be
vinny <vinicios_neves@hotmail.com>
2017-11-22 23:03:50 -0200

    iniciando estrutura do projeto

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <MainHeader/>
+  </div>
+</template>
+
+<script>
+import MainHeader from './components/root/MainHeader';
+
+export default {
+  name: 'app',
+  components: {
+    MainHeader,
+  },
+};
+</script>
+
+<style src="@/assets/sass/app.scss" lang="sass">
+
+</style>
